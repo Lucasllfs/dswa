3028023d12b9000e23af18482cee069ffbf3c686
Lucaslfs <lucas.limalfss@gmail.com>
2025-02-10 09:39:05 -0300

    Atualiza nomes dos membros na lista: altera "Lucas Goes" para "Lucas G Dias" e "Andre Luis" para "Andre Luis #2"

--- src/ARQUIVOS PARA SEREM ALTERADOS PARA O SITE FUNCIONAR/teamName-membros.ts
+++ src/ARQUIVOS PARA SEREM ALTERADOS PARA O SITE FUNCIONAR/teamName-membros.ts
@@ -10,7 +10,7 @@ export const MEMBER_NAMES = ["Rick O Robert",
     "Wilker Silva Ribeiro",
     "Mateus Matsuo",
     "Felipe Bonadia",
-    "Lucas Goes",
-    "Andre Luis",
+    "Lucas G Dias ",
+    "Andre Luis #2",
     "Yukaa"
 ];
